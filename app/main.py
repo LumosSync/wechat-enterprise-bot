3e7a5f946761c48146250d0edc99248c2a447a08
Color Autumn <colorautumn@outlook.com>
2025-05-07 08:44:16 +0000

    🎉Init project

---
+++ app/main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from .routers import items
+
+app = FastAPI()
+app.include_router(items.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello FastAPI"}
