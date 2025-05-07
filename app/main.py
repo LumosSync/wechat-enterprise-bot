770b10dd7d815453c6699c150a08baa63377da20
17TheWord <17theword@gmail.com>
2025-05-07 23:22:14 +0800

    feat(wechat): 添加企业微信消息加解密功能
    - 新增 WXBizMsgCrypt 类实现消息加解密
    - 添加 XMLParse 类处理 XML 消息格式
    - 实现 PKCS7Encoder 类提供填充补位功能
    - 新增 ierror.py 定义错误码含义

--- app/main.py
+++ app/main.py
@@ -5,6 +5,12 @@ from .routers import items
 app = FastAPI()
 app.include_router(items.router)
 
+
+sToken = "hJqcu3uJ9Tn2gXPmxx2w9kkCkCE2EPYo"
+sEncodingAESKey = "6qkdMrq68nTKduznJYO1A37W2oEgpkMUvkttRToqhUt"
+sCorpID = "ww1436e0e65a779aee"
+
 @app.get("/")
-async def root():
+async def root(data):
+    print(data)
     return {"message": "Hello FastAPI"}
